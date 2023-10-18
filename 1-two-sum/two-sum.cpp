06f9981ae4f4e6c9a6176612ff5cc0811952b5c4
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2023-10-18 17:28:02 -0400

    Time: 726 ms (5.01%) | Memory: 10.5 MB (73.71%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -8,10 +8,11 @@ public:
         vector<int> solution;
 
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i != j && nums[i] + nums[j] == target) {
                     solution.push_back(i);
                     solution.push_back(j);
+                     return solution;
                 }
             }
         }
