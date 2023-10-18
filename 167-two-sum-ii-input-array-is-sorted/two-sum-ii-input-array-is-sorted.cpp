50548f54f55c34cd98cdec801de404de5a7cc8ae
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2023-10-18 18:05:49 -0400

    Time: 14 ms (20.88%) | Memory: 16 MB (31.26%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+        // Time: O(n)
+        // Space: O(1)
+
         int low = 0;
         int high = numbers.size() - 1;
-        // vector<int> solution;
+        vector<int> solution;
 
         while(true) {
-            // int current = numbers[low] + numbers[high];
-
             if (numbers[low] + numbers[high] == target) {
-                // solution.push_back(low + 1);
-                // solution.push_back(high + 1);
-                vector<int> solution {low +1, high+1};
-                return solution;
+                solution.push_back(low + 1);
+                solution.push_back(high + 1);
+                        return solution;
             }
             if (numbers[low] + numbers[high] < target) {
                 low++;
@@ -20,6 +20,6 @@ public:
                 high--;
             }
         }
-        return vector <int> ();
+        return solution;
     }
 };
