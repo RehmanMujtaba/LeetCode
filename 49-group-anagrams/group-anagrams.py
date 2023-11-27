d169cf4d26fe38425525b332400e6017f9721f15
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2023-11-27 14:41:50 -0500

    Time: 123 ms (10.15%) | Memory: 22.4 MB (11.32%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -8,13 +8,6 @@ class Solution:
             for letter in item:
                 counts_array[ord("z") - ord(letter)] += 1
             
-            if tuple(counts_array) in hashmap:
-                hashmap[tuple(counts_array)].insert(0, item)
-            else:
-                hashmap[tuple(counts_array)] = [item]
+            hashmap[tuple(counts_array)].append(item)
 
-        answer = []
-        for key, value in hashmap.items():
-            answer.insert(0, value)
-            
-        return answer
+        return hashmap.values()
