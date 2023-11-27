38632b8e98f40c6721d802f63db69ac89a757035
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2023-11-27 14:37:03 -0500

    Time: 130 ms (8.26%) | Memory: 21.3 MB (34.35%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,39 +1,5 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-#         # Sort all strings in List
-#         # Make empty List of list return value
-#         # Iterate through List
-#             # Keep first element as a target
-#             # Check if next element is equal to tareget
-#                 # If equal, add to SubList
-#                 # If not, then set equal to new target
-#         # Return list of list
-
-#         sorted_list = []
-
-#         for item in strs:
-#             new_item = ''.join(sorted(item))
-#             sorted_list.insert(0, new_item)
-        
-#         sorted_list.sort()
-#         strs.sort()
-
-#         newList = []
-        
-#         target = sorted_list[0]
-#         targetList = [strs[0]] 
-
-#         for item, sorted_item in zip(strs, sorted_list):
-#             if sorted_item == target:
-#                 targetList.insert(0, item)
-#             else:
-#                 target = sorted_item
-#                 newList.insert(0, targetList)
-#                 targetList = [item]
-        
-#         return newList
-
         hashmap = {}
 
         for item in strs:
@@ -52,11 +18,3 @@ class Solution:
             answer.insert(0, value)
             
         return answer
-
-
-
-
-        
-
-
-
