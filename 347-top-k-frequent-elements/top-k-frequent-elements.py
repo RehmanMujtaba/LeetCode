b1618feeb3f16d8d273ce031c748f0d3d67948a2
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2023-11-27 20:44:34 -0500

    Time: 96 ms (71.88%) | Memory: 21 MB (73.84%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,21 +1,31 @@
+import heapq
+from typing import List
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
         hashmap = {}
 
+        # Count the frequency of each element
         for item in nums:
-            hashmap[item] = 1 + hashmap.get(item,0)
-        
-        counts = [[] for item in range(len(nums) + 1)]
-        for key,value in hashmap.items():
-            counts[value].append(key)
-        
-        answer = []
-        for arr in reversed(counts):
-            if k > 0:
-                for item in arr:
-                    answer.append(item)
-                    k -= 1
+            if item in hashmap:
+                hashmap[item] += 1
             else:
-                return answer
-        return answer
+                hashmap[item] = 1
+
+        # Use a min heap to keep track of the k most frequent elements
+        min_heap = []
+
+        for key, value in hashmap.items():
+            # Push the first k elements into the heap
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, (value, key))
+            else:
+                # If the current element has a higher frequency than the smallest element in the heap, replace it
+                if value > min_heap[0][0]:
+                    heapq.heappop(min_heap)
+                    heapq.heappush(min_heap, (value, key))
+
+        # Extract the elements from the heap
+        result = [element[1] for element in min_heap]
+
+        return result
