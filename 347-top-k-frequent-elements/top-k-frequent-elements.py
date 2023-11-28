bec04330d4a17c91185237215d47bf016bb924d5
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2023-11-27 20:55:31 -0500

    Time: 95 ms (76.30%) | Memory: 21.1 MB (54.81%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -13,19 +13,10 @@ class Solution:
                 hashmap[item] = 1
 
         # Use a min heap to keep track of the k most frequent elements
-        min_heap = []
+        max_heap = [(-value, key) for key, value in hashmap.items()]
+        heapq.heapify(max_heap)
 
-        for key, value in hashmap.items():
-            # Push the first k elements into the heap
-            if len(min_heap) < k:
-                heapq.heappush(min_heap, (value, key))
-            else:
-                # If the current element has a higher frequency than the smallest element in the heap, replace it
-                if value > min_heap[0][0]:
-                    heapq.heappop(min_heap)
-                    heapq.heappush(min_heap, (value, key))
-
-        # Extract the elements from the heap
-        result = [element[1] for element in min_heap]
+        # Extract the k most frequent elements from the max heap
+        result = [heapq.heappop(max_heap)[1] for _ in range(k)]
 
         return result
