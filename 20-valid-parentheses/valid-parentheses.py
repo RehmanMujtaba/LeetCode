b33ab727199d21acba2410c360bfa096ae2b36c3
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2023-11-30 14:20:48 -0500

    Time: 38 ms (60.65%) | Memory: 16.2 MB (61.73%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,25 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        table = {")" : "(", "}" : "{", "]" : "["}
 
         for x in s:
             if x == "(" or x == "[" or x == "{":
                 stack.append(x)
-            if x == "}":
-                if  len(stack) >= 1 and stack[-1] == "{":
-                    stack.pop()
-                else:
-                    return False
-            if x == ")":
-                if  len(stack) >= 1 and stack[-1] == "(" :
-                    stack.pop()
-                else:
-                    return False
-            if x == "]":
-                if  len(stack) >= 1  and stack[-1] == "[":
-                    stack.pop()
-                else:
-                    return False
+            elif len(stack) > 0 and stack[-1] == table[x]:
+                stack.pop()
+                continue
+            else:
+                return False
                 
         if len(stack) == 0:
             return True
