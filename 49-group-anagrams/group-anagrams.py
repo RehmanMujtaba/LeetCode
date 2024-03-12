bc2f70f9bf012d37b32c92668609ae14c9e20fa8
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-03-12 11:40:11 -0400

    Time: 151 ms (5.05%) | Memory: 28.2 MB (5.01%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,6 +1,26 @@
+import hashlib
+import base64
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
+    
+        def make_hash_sha256(o):
+            hasher = hashlib.sha256()
+            hasher.update(repr(make_hashable(o)).encode())
+            return base64.b64encode(hasher.digest()).decode()
+
+        def make_hashable(o):
+            if isinstance(o, (tuple, list)):
+                return tuple((make_hashable(e) for e in o))
+
+            if isinstance(o, dict):
+                return tuple(sorted((k,make_hashable(v)) for k,v in o.items()))
+
+            if isinstance(o, (set, frozenset)):
+                return tuple(sorted(make_hashable(e) for e in o))
+
+            return o
+
         hm = {}
 
         for s in strs:
@@ -11,10 +31,10 @@ class Solution:
                 else:
                     newMap[letter] = 1
             #print(newMap)
-            if json.dumps(newMap, sort_keys=True) in hm:
+            if make_hashable(newMap) in hm:
                 #print(f"{newMap} is in {hm}")
-                hm[json.dumps(newMap, sort_keys=True)].extend([s])
+                hm[make_hashable(newMap)].extend([s])
             else:
-                hm[json.dumps(newMap, sort_keys=True)] = [s]
+                hm[make_hashable(newMap)] = [s]
         
         return hm.values()
