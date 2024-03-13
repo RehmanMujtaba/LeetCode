3cf5e18be10e304c2add9e11d07938744cdc2e05
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-03-13 15:43:42 -0400

    Time: 39 ms (41.01%) | Memory: 16.5 MB (85.73%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,19 +1,19 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        
-        reverse = digits[::-1]
-        
+                
         def increment(pos : int):
-            if reverse[pos] != 9:
-                reverse[pos] += 1
+            newPos = - 1 - pos
+            print(newPos)
+            if digits[newPos] != 9:
+                digits[newPos] += 1
                 return
-            elif pos < len(reverse) - 1:
-                reverse[pos] = 0
+            elif pos < len(digits) - 1:
+                digits[newPos] = 0
                 increment(pos + 1)
             else:
-                reverse[pos] = 0
-                reverse.append(1)
+                digits[newPos] = 0
+                digits.insert(0, 1)
                 return
         increment(0)
-        return reverse[::-1]
+        return digits
             
