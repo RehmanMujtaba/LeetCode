ae7ee7763b43b6a78d45879fe5ee984a851c8420
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-03-13 15:16:31 -0400

    Time: 101 ms (15.82%) | Memory: 16.5 MB (68.44%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -2,6 +2,7 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
         def isValidSection(sec: List[str]) -> bool:
+            print(sec)
             hp = set()
             for num in sec:
                 if (num in hp) and (num != "."):
@@ -20,63 +21,15 @@ class Solution:
                 lst.append(board[j][i])
             if not isValidSection(lst):
                 return False
-        for j in range(0,9,3):
-            lst = []
-            for i in range(0,3):
-                lst.append(board[i][j + 0])
-                lst.append(board[i][j + 1])
-                lst.append(board[i][j + 2])
-            if not isValidSection(lst):
-                return False
         
-        lst = []
-        j = 0
-        for i in range(3,6):
-            lst.append(board[i][j + 0])
-            lst.append(board[i][j + 1])
-            lst.append(board[i][j + 2])
-        if not isValidSection(lst):
-            return False
-        lst = []
-        j = 3
-        for i in range(3,6):
-            lst.append(board[i][j + 0])
-            lst.append(board[i][j + 1])
-            lst.append(board[i][j + 2])
-        if not isValidSection(lst):
-            return False
-        lst = []
-        j = 6
-        for i in range(3,6):
-            lst.append(board[i][j + 0])
-            lst.append(board[i][j + 1])
-            lst.append(board[i][j + 2])
-        if not isValidSection(lst):
-            return False
-        lst = []
-    
-        j = 0
-        for i in range(6,9):
-            lst.append(board[i][j + 0])
-            lst.append(board[i][j + 1])
-            lst.append(board[i][j + 2])
-        if not isValidSection(lst):
-            return False
-        lst = []
-        j = 3
-        for i in range(6,9):
-            lst.append(board[i][j + 0])
-            lst.append(board[i][j + 1])
-            lst.append(board[i][j + 2])
-        if not isValidSection(lst):
-            return False
-        lst = []
-        j = 6
-        for i in range(6,9):
-            lst.append(board[i][j + 0])
-            lst.append(board[i][j + 1])
-            lst.append(board[i][j + 2])
-        if not isValidSection(lst):
-            return False
+        for k in range(0, 9, 3):
+            for j in range(0,9,3):
+                lst = []
+                for i in range(k,k+3):
+                    lst.append(board[i][j + 0])
+                    lst.append(board[i][j + 1])
+                    lst.append(board[i][j + 2])
+                if not isValidSection(lst):
+                    return False
 
         return True
