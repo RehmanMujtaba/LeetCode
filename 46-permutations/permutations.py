5a410ab1eee3239b96108b10bff0f98ccd6f87f9
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-03-25 21:00:41 -0400

    Time: 47 ms (16.89%) | Memory: 16.8 MB (36.58%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,12 +1,14 @@
-class Solution:
+from typing import List
+class Solution:  
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0:
-            return [[]] 
-        else:
-            result = []
-            for i in range(len(nums)):
-                current_num = nums[i]
-                remaining_nums = nums[:i] + nums[i+1:]  
-                for perm in self.permute(remaining_nums):   
-                    result.append([current_num] + perm) 
-            return result
+        def backtrack(remaining_nums, permutation):
+            if not remaining_nums:
+                ans.append(permutation)
+                return
+
+            for i in range(len(remaining_nums)):
+                backtrack(remaining_nums[:i] + remaining_nums[i+1:], permutation + [remaining_nums[i]])
+
+        ans = []
+        backtrack(nums, [])
+        return ans
