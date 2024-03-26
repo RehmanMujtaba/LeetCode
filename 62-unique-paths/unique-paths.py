6dcc1a47618380ddcb5cd5d1aaf7a9cef9917c86
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-03-25 22:45:56 -0400

    Time: 42 ms (15.56%) | Memory: 16.6 MB (17.56%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,4 +1,5 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         total = (m - 1) + (n - 1)
-        return int((math.factorial(total))/((math.factorial(total - m + 1) * math.factorial(m - 1))))
+        num = min(m,n) - 1
+        return int((math.factorial(total))/((math.factorial(total - num) * math.factorial(num))))
