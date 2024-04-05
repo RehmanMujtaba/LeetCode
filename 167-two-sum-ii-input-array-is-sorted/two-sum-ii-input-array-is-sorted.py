0ce9f67fc76b86a6fc6a73ed59046a40837cd029
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-04-05 13:27:19 -0400

    Time: 113 ms (18.93%) | Memory: 17.4 MB (57.72%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,27 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        f = open("user.out", 'w')
-        print("[1,2]", file=f)
-        print("[1,3]", file=f)
-        print("[1,2]", file=f)
-        print("[1,2]", file=f)
-        print("[1,2]", file=f)
-        print("[2,3]", file=f)
-        print("[3,6]", file=f)
-        print("[4,5]", file=f)
-        print("[3,4]", file=f)
-        print("[3,4]", file=f)
-        print("[24,32]", file=f)
-        print("[3,42]", file=f)
-        print("[24,30]", file=f)
-        print("[134,496]", file=f)
-        print("[20,87]", file=f)
-        print("[6,34]", file=f)
-        print("[4,8]", file=f)
-        print("[135,441]", file=f)
-        print("[13011,13012]", file=f)
-        print("[29999,30000]",file=f)
-        print("[1,2]",file=f)
-        print("[3,5]",file=f)
-        print("[3,5]",file=f)
-        exit(0) 
+        l, r = 0, len(numbers) - 1
+
+        while True :
+            if (numbers[l] + numbers[r] == target):
+                return [l + 1, r + 1]
+            elif (numbers[l] + numbers[r] < target):
+               l += 1
+            else:
+                r -= 1
+        
