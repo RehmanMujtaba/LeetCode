83a8a9d1a931e8f126e2934161d2bd98a3ef017e
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-04-05 16:28:28 -0400

    Time: 628 ms (24.95%) | Memory: 23 MB (61.81%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -6,9 +6,9 @@ class Solution:
         sol = []
 
         for point in points:
-            dist = pow(point[0], 2) + pow(point[1], 2)
             x = point[0]
             y = point[1]
+            dist = pow(x, 2) + pow(y, 2)
             if len(heap) < k:
                 heappush(heap, [-1 * dist, x, y])
             elif heap[0][0] < -1 * dist:
