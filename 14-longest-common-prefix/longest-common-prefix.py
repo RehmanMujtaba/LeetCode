37c4352ee4ffdce0e6ca3130042b69f45db85c55
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-04-09 16:37:20 -0400

    Time: 42 ms (28.68%) | Memory: 16.8 MB (10.06%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,11 +1,14 @@
+import random
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
         prefix = ""
         index = 0
+        randomNum = random.randint(0, len(strs) - 1)
 
-        while len(prefix) < len(strs[0]):  
-            char = strs[0][index]          
+        while len(prefix) < len(strs[randomNum]):  
+            char = strs[randomNum][index]          
             for string in strs:
                 if index < len(string) and char == string[index]:
                     None
