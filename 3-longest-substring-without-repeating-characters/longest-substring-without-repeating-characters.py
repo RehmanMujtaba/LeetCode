7cb643c54d89cb46c43028cdd7f0be16c8171cc8
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-04-09 15:59:23 -0400

    Time: 506 ms (5.10%) | Memory: 16.7 MB (60.51%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,16 +4,16 @@ class Solution:
         maxLen = 0
         l, r = 0, 0
         hashset = ""
+        length = len(s)
 
-        while r < len(s):       
+        while r < length:       
             if s[r] in hashset:
                 l += 1
                 r = l               
                 hashset = ""              
             else:
                 hashset += s[r]
-                r += 1
-        
+                r += 1      
             maxLen = max(maxLen, r - l)
 
         return maxLen
