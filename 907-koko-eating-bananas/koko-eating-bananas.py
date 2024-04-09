fb6d9cc76ffc6096cdec67e03112686f7d4042b9
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-04-09 11:42:57 -0400

    Time: 323 ms (12.73%) | Memory: 18 MB (92.35%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        minSpeed = 1
+        maxSpeed = max(piles)
+
+        while minSpeed < maxSpeed:
+            temp = piles
+            hours = 0
+            currSpeed = (minSpeed + maxSpeed) // 2
+            print(f"Left: {minSpeed} Right: {maxSpeed} Mid: {currSpeed}")
+            for i in range(0, len(temp)):
+                hours += math.ceil(temp[i] / currSpeed)
+                if hours > h:
+                    minSpeed = currSpeed + 1
+                    break
+            else:
+                maxSpeed = currSpeed
+
+        return minSpeed        
+        
