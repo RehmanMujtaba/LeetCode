eddb8a5fd524c1798c764c3cb147ee272ae4e348
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-04-09 11:50:06 -0400

    Time: 299 ms (23.49%) | Memory: 18.2 MB (57.91%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -9,8 +9,8 @@ class Solution:
             temp = piles
             hours = 0
             currSpeed = (minSpeed + maxSpeed) // 2
-            for i in range(0, length):
-                hours += math.ceil(temp[i] / currSpeed)
+            for pile in piles:
+                hours += math.ceil(pile / currSpeed)
                 if hours > h:
                     minSpeed = currSpeed + 1
                     break
