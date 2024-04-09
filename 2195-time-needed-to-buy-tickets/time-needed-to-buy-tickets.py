8ab9ed0192c9be2201f3f0ff6b2afd671a1d7211
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-04-09 11:59:53 -0400

    Time: 31 ms (98.12%) | Memory: 16.4 MB (92.60%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,16 +1,12 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        
         seconds = 0
-        target = tickets[k]
-
         for index, count in enumerate(tickets):
-            if count < target:
+            if count < tickets[k]:
                 seconds += count
+            elif index < k:
+                seconds += tickets[k]
             else:
-                if index > k:
-                    seconds += target - 1
-                else: 
-                    seconds += target
-            
-        return seconds
+                seconds += tickets[k] - 1
+
+        return seconds + 1
