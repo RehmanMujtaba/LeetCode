31a468e29806a479c041b279b07f8c2571465a03
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-04-09 15:34:51 -0400

    Time: 59 ms (32.83%) | Memory: 16.8 MB (9.91%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -6,34 +6,40 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        dummy = ListNode()
-        curr_node = dummy
-        curr_1 = l1
-        curr_2 = l2
+        head = ListNode() # Dummy Head
+        curr = head
+        node = None
+
+        l1_curr = l1
+        l2_curr = l2
+
         carry = False
 
-        while curr_1 != None or curr_2 != None:
-            digit = 0
-            if curr_1:
-                digit = curr_1.val
-                curr_1 = curr_1.next
-            if curr_2:
-                digit += curr_2.val
-                curr_2 = curr_2.next
-            if carry:
-                digit += 1
-                carry = False            
-            if digit >= 10:
-                digit = digit % 10
+        while l1_curr or l2_curr:
+            if l1_curr and l2_curr:
+                sum = l1_curr.val + l2_curr.val + carry
+                l1_curr = l1_curr.next
+                l2_curr = l2_curr.next
+            elif l1_curr:
+                sum = l1_curr.val + carry
+                l1_curr = l1_curr.next 
+            else:
+                sum = l2_curr.val + carry
+                l2_curr = l2_curr.next
+                        
+            if sum >= 10:
+                node = ListNode(sum % 10)
                 carry = True
+            else:
+                node = ListNode(sum)
+                carry = False
             
-            new_node = ListNode(digit, None)
-            curr_node.next = new_node
-            curr_node = new_node
-            
+            curr.next = node
+            curr = curr.next
         
         if carry:
-            new_node = ListNode(1, None)
-            curr_node.next = new_node
+            curr.next = ListNode(1)
+        
+        return head.next
 
-        return dummy.next
+
