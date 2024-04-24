758dcecc4b8a0e3826c9d62f683d0f3cc68ac106
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-04-24 10:14:22 -0400

    Time: 40 ms (18.45%) | Memory: 16.5 MB (31.32%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,16 +1,19 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1 or n == 2:
-            return 1
-        
-        a, b, c = 0, 1, 1
-        
-        for i in range(3, n + 1):
-            next_trib = a + b + c
-            a = b
-            b = c
-            c = next_trib
-        
-        return c
+        if n == 0: return 0
+        if n == 1: return 1
+        if n == 2: return 1
+
+        trib = [-1]*(n+1)
+        trib[0] = 0
+        trib[1] = 1
+        trib[2] = 1
+        def getTrib(idx):
+            if trib[idx] == -1: # recursive case
+                if trib[idx-1] == -1: trib[idx-1] = getTrib(idx-1) 
+                if trib[idx-2] == -1: trib[idx-2] = getTrib(idx-2) 
+                if trib[idx-3] == -1: trib[idx-3] = getTrib(idx-3)            
+                return  trib[idx-1] + trib[idx-2] + trib[idx-3]
+            else: # base case   
+                return trib[idx]
+        return getTrib(n)  
