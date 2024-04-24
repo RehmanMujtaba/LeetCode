5b203ff26a7e45723c4ea5a47051aa49d6f4585a
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-04-24 09:24:06 -0400

    Time: 37 ms (38.81%) | Memory: 16.5 MB (85.24%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -4,8 +4,9 @@ class Solution:
     def tribonacci(self, n: int) -> int: 
         def helper(n, map):
             if n in map: return map[n]
-            map[n] = helper(n - 3, map) + helper(n - 2, map) + helper(n - 1, map)
-            return map[n]
+            result = helper(n - 1, map) + helper(n - 2, map) + helper(n - 3, map)
+            map[n] = result
+            return result
 
         map = {}
         map[0] = 0
