ead3c30bcfd229ce840e805604c2d509254ab8e0
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-05-24 14:30:18 -0400

    Time: 710 ms (21.00%) | Memory: 34.3 MB (31.27%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -4,15 +4,13 @@ class Solution:
         stack = []
         
         for index, height in enumerate(heights):
-            if len(stack) == 0:
-                stack.append([height, index])    
-            else:        
-                i = index
-                while len(stack) > 0 and stack[-1][0] > height:
-                        h, i = stack.pop()
-                        area = h * (index - i)
-                        curr_max = max(curr_max, area)
-                stack.append([height, i])
+
+            i = index
+            while len(stack) > 0 and stack[-1][0] > height:
+                    h, i = stack.pop()
+                    area = h * (index - i)
+                    curr_max = max(curr_max, area)
+            stack.append([height, i])
         
         while len(stack) is not 0:
             height, index = stack.pop()
