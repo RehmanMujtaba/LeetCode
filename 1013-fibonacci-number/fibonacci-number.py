93075b7f815f586cc0615aaba5f8fa2c91825cf8
Mujtaba Rehman <97486476+RehmanMujtaba@users.noreply.github.com>
2024-06-11 21:02:17 -0400

    Time: 37 ms (60.87%) | Memory: 16.5 MB (69.98%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,18 @@
 class Solution:
-    def fib(self, n: int) -> int:
 
-        if n == 0 or n == 1:
-            return n
-            
-        return self.fib(n-1) + self.fib(n-2)
+    def fib(self, n: int) -> int:
+        visited = [False] * (n + 1)
+        answer = [0] * (n + 1)
         
+        def recurse(n: int) -> int:
+            if visited[n] == True:
+                return answer[n]
+
+            if n == 0 or n == 1:
+                return n
+             
+            answer[n] = recurse(n-1) + recurse(n-2)
+            visited[n] = True
+            return answer[n]
+
+        return recurse(n)
